3a184bf74ecb96ab33e74908afec92a369dad69f
Luca Bilke <luca@gmail.com>
2023-02-09 18:21:57 +0100

    generalization

--- config.h
+++ config.h
@@ -2,11 +2,11 @@
 static const Block blocks[] = {
   /*Icon*//*Command*/       /*Update Interval*/ /*Update Signal*/
   {"", "cat /tmp/recordingicon 2>/dev/null", 0,      9},
-  {"", "sb-pacpackages",                     0,      8},
+  {"", "sb-packages",                        0,      8},
   {"", "sb-xkbmap",                          0,      15},
   {"", "sb-forecast",                        3600,   5},
   // {"", "sb-tasks",                           10,     26},
-  {"", "sb-playerctl",                           0,      11},
+  {"", "sb-playerctl",                       0,      11},
   // {"", "sb-news",                            0,      6},
   // {"", "sb-torrent 192.168.178.200",         20,     7},
   // {"", "sb-mailbox",                         180,    12},
@@ -15,8 +15,8 @@ static const Block blocks[] = {
   // {"", "sb-ip",                              5,      13},
   {"", "sb-internet",                        5,      4},
   {"", "sb-nettraf",                         1,      16},
-  // {"", "sb-battery",                         5,      3},
-  {"", "sb-volume",                          0,      10},
+  {"", "sb-battery",                         5,      3},
+  // {"", "sb-volume",                          0,      10},
   {"", "sb-clock",                           60,     1},
 };
 
